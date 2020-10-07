c7139d4e22df8b221d22b748616c7bb4788d82fc
Deepanjan Nag <subscriptions@deepanjannag.com>
2020-10-07 12:52:49 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvitesCovid.Models;
+
+namespace PartyInvitesCovid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good MOrning!" : "Good Afternoon";
+            return View("MyView");
+        }
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
